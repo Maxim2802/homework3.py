a59e239b18e7a206a0a2980c54dc4cb5b26f7d63
Maxim2802 <maximtchebykin@yandex.ru>
2024-05-30 20:27:15 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,12 @@
+# Практическая работа по уроку "Динамическая типизация"
+
+
+name = "Максим"
+print("Name: " + name, type(name))
+age = 28
+print("Age: " + str(age), type(age))
+age = age + 7
+print("New Age: " + str(age), type(age))
+is_student = True
+print("Is Student: " + str(is_student), type(is_student))
+
